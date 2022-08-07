db65efc145fb5eccd75b3a906d4edc32a6467795
kiamelJ <90395373+kiamelJ@users.noreply.github.com>
2022-08-07 16:55:41 +0200

    done up till portfolio

---
+++ src/components/Contact/index.js
@@ -0,0 +1,115 @@
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect, useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_rb86o9f',
+        'template_kfubsae',
+        form.current,
+        'Mxg4xl9coLs15ayW5'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className='container contact-page'>
+        <div className='text-zone'>
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially ambitious or
+            large projects. However, if you have other request or question,
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className='contact-form'>
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className='half'>
+                  <input type='text' name='name' placeholder='Name' required />
+                </li>
+                <li className='half'>
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='Email'
+                    required
+                  />
+                </li>
+
+                <li>
+                  <input
+                    type='text'
+                    name='subject'
+                    placeholder='Subject'
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    name='message'
+                    placeholder='Message'
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type='submit' className='flat-button' value='SEND' />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className='info-map'>
+          Mikael Jonsson
+          <br />
+          Sweden,
+          <br />
+          Härbrevägen 42 <br />
+          Örnsköldsvik <br />
+          <span>jogajoga@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[63.31398, 18.76474]} zoom={13}>
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker position={[63.31398, 18.76474]}>
+              <Popup>Mike lives here!</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type='pacman' />
+    </>
+  )
+}
+
+export default Contact
---
+++ src/components/Home/index.js
@@ -0,0 +1,77 @@
+import { isEditable } from '@testing-library/user-event/dist/utils'
+import { useState, useEffect } from 'react'
+import Loader from 'react-loaders'
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import Logo from './Logo'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  // Får inte det här att funka men skit i det för jag vill nog ändå inte ha det
+  // Osäker på om jag vill ha nån effekt alls här
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className='container home-page'>
+        <div className='text-zone'>
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt='JavaScript Developer Name, Web Developer Name'
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to='/contact' className='flat-button'>
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
+      <Loader type='pacman' />
+    </>
+  )
+}
+
+export default Home
